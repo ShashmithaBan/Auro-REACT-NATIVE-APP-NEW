fba673f8b626f1a31149f4313decdfed9b077d70
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-16 03:10:58 +0530

    Initial commit with project files

---
+++ app/_layout.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const _layout = () => {
+  return (
+    <View className = 'flex-1 text-4xl justify-center items-center'>
+      <Text className="text-4xl fo">_layout</Text>
+      <Text>_layout</Text> 
+    </View>
+  )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({})
