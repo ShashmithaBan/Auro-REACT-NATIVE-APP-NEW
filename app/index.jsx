b277bb7203be5e3636864829b0af2ef2d20a9590
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-16 21:52:07 +0530

    updated

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text className=' text-4xl font-pbold'>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
