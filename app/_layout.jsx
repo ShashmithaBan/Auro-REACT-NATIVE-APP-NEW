906fe0fcd937c782992bdb86bf02b6cd34e7f237
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-17 02:08:47 +0530

    updated

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,31 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useFonts } from "expo-font";
-import { Stack } from 'expo-router';
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
     'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
-    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
-
   });
 
 
 
   return (
-   <Stack>
-    <Stack.Screen name='index' options={{headerShown:false}}/>
-   </Stack>
-  
+    <View className = 'flex-1 text-4xl justify-center items-center'>
+      <Text className="font-bold">_layout2</Text>
+      <Text>_layout</Text> 
+    </View>
   )
 }
 
 export default RootLayout;
-
