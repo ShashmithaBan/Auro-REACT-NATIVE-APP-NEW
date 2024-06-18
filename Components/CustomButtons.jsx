c2fa80ba3a6327b6e777652d6c6aa743f07bf77a
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-19 02:41:09 +0530

    implemented the onboarding screen

---
+++ Components/CustomButtons.jsx
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CustomButtons = ({title,handlePress , containerStyles , textStyles , isLoading}) => {
+  return (
+    <TouchableOpacity
+    onPress={handlePress}
+    activeOpacity={0.7}   
+    className = {`mt-5 bg-secondary-100 min-h-[62px] min-w-[250px] rounded-xl justify-center items-center ${containerStyles}
+    ${isLoading ? 'opacity-50':''} `}
+    disabled={isLoading}
+    >
+      <Text className={`text-center text-lg font-psemibold text-white ${textStyles}`}>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButtons
+
+
+// activeOpacity is like button hover
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./Components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors:{
