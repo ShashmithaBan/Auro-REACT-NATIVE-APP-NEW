8e8d881ce6378ec50dbc021526442662252d0ebd
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-07-13 09:22:09 +0530

    updated

---
+++ Components/VideoCard.jsx
@@ -0,0 +1,35 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { icons } from '../constants';
+
+const VideoCard = ({ video: { title,  thumbnail , video , creator: { username, avatar } } }) => {
+    const [play, setPlay] = useState(false)
+  return (
+    <View className='flex-col items-center px-4 mb-14'>
+        <View className='flex-row gap-3 items-start'>
+            <View className='items-center justify-center flex-row flex-1 gap-2'>
+            <View className='w-[46px] h-[46px] border-secondary items-center p-0.5 border rounded-full'>
+                <Image
+                source={{uri:avatar}}
+                className='w-full h-full rounded-full'
+                resizeMode='cover'
+                />
+            </View>
+            <View className ='flex-col flex-1 gap-y-1 ml-2  '>
+            <Text className='h-[20px] text-white' numberOfLines={1}>{title}</Text>
+            <Text className='text-white'>{username}</Text>
+            </View>
+            </View>
+           <View className='pt-4'>
+           <Image source={icons.menu} className='h-5 w-6' resizeMode='contain'/>
+           </View>
+            
+        </View>
+       {play }
+    </View>
+  );
+};
+
+
+
+export default VideoCard;
